254a842fe024d75a1c23dd7daa58f1fc773baee1
Bobby Dewan Akram Khaleque <b.khaleque@se16.qmul.ac.uk>
2018-11-19 16:46:49 +0000

    First commit

--- form/views.py
+++ form/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse,QueryDict
 from .models import  Product
 from django.template import RequestContext
 from .forms import ProductForm
 import json
 from django.views.decorators.csrf import csrf_exempt
+
+
 
 
 
@@ -15,9 +17,10 @@ from django.views.decorators.csrf import csrf_exempt
 
    # def post(self, request):
 
-
+@csrf_exempt
 def index(request):
     return render(request, 'form/index.html')
+
 
 @csrf_exempt
 def products(request):
@@ -30,37 +33,37 @@ def products(request):
                          'Price': p['product_price']}
             output_list.append(json_dict)
         if request.method == 'GET':
-
             return JsonResponse(output_list, safe=False,)
 
         elif request.method == 'DELETE':
-            body = json.loads(request.body.decode('utf-8'))
-            product = Product.objects.get(product_name=body['id'])
-            product.delete()
-            return JsonResponse(output_list, safe=False)
+            ID = QueryDict(request.body).get('ID')
+            p = Product.objects.get(pk=ID)
+            p.delete()
+            return JsonResponse(None, safe=False)
 
         elif request.method == 'POST':
-            body = json.loads(request.body.decode('utf-8'))
-            product = Product.objects.create()
-            product.name = body['name']
-            product.desc = body['description']
-            product.price = body['price']
-            product.save()
-            return JsonResponse(output_list, safe=False)
+            p = Product()
+            p.product_name = QueryDict(request.body).get('Name')
+            p.product_desc = QueryDict(request.body).get('Description')
+            p.product_price = QueryDict(request.body).get('Price')
+            p.save()
+            return JsonResponse(p.id, safe=False)
 
         elif request.method == 'PUT':
-            body = json.loads(request.body.decode('utf-8'))
-            product = Product.objects.get(id=request.ID)
-            product.name = body['name']
-            product.description = body['description']
-            product.price = body['price']
-            product.save()
-            data = Product.objects.all().values()
-            return JsonResponse(list(data), safe=False)
+            ID = QueryDict(request.body).get('ID')
+            p = Product.objects.get(pk=ID)
+            name = QueryDict(request.body).get('Name')
+            desc = QueryDict(request.body).get('Description')
+            price = QueryDict(request.body).get('Price')
+            p.product_name = name
+            p.product_desc = desc
+            p.product_price = price
+            p.save()
+            output_list = serializers.serialize("json", [p])
+            return JsonResponse(output_list, safe=False)
 
-
+@csrf_exempt
 def product_details(request, product_id):
-    #response = "You're looking at the details of question %s."
     if request.method =='GET':
         product = get_object_or_404(Product, pk=product_id)
 
